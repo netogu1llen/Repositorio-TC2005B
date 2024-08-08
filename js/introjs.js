d78e6f873240d5516001df5e72e6c69fa791a2eb
Netogu1llen <pepeppolloxd12@gmail.com>
2024-08-08 12:43:27 -0600

    variables, let y cons

--- js/introjs.js
+++ js/introjs.js
@@ -6,12 +6,23 @@ console.warn("cuidado");
 console.error("Te equivocaste");
 console.assert(1==0);
 
-//variables, constantes
+//Variables, constantes
 
 //let permite declarar variables
 let nombre="Ernesto";
 console.log("Hola"+ nombre)
 console.log(`${nombre} está programando y con hambre`)
-
 //const declara constantes
 const precio_chocolate=89;
+
+
+//Alcance de las variables
+
+{
+    let precio_leche=25;
+}
+
+/*Arroja error porque precio crema  fue declarada en 
+otro ámbito y finalizó 2 lineas arriba*/
+console.log(precio_leche);
+
