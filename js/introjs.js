a1a0766f2ddad4847d73f0506ee4c35012facd44
Netogu1llen <pepeppolloxd12@gmail.com>
2024-08-08 12:42:39 -0600

    variables, let y cons

---
+++ js/introjs.js
@@ -0,0 +1,17 @@
+//consola (log, ingo, warn, error, assert)
+
+console.log("hola mundo");
+console.info("esto es información importante");
+console.warn("cuidado");
+console.error("Te equivocaste");
+console.assert(1==0);
+
+//variables, constantes
+
+//let permite declarar variables
+let nombre="Ernesto";
+console.log("Hola"+ nombre)
+console.log(`${nombre} está programando y con hambre`)
+
+//const declara constantes
+const precio_chocolate=89;
