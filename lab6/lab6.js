3b75c0474db9a852c48433cf51c86da436752065
Netogu1llen <pepeppolloxd12@gmail.com>
2024-08-12 12:03:17 -0600

    Lab6 imagen

---
+++ lab6/lab6.js
@@ -0,0 +1,8 @@
+const boton=document.getElementById("boton_chocolate");
+
+boton.onclick=()=>{
+    const imagen=document.getElementById("imagen_chocolate");
+    imagen.alt="Imagen de vaciado de chocolate";
+    imagen.src="https://imgmedia.buenazo.pe/650x358/buenazo/original/2020/08/19/5f3dbfcf10163977e21ab8a2.jpg"
+};
+
